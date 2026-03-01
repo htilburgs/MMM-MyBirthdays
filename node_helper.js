d365d1eadd89e87b5be217181696ba254205037f
Harm Tilburgs <harm@tilburgs.com>
2026-03-01 09:08:14 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -27,33 +27,32 @@ module.exports = NodeHelper.create({
     },
 
     startWebServer: function () {
-        const app = express();
+        const app = this.expressApp;
+
         app.use(express.json());
 
-        // Serveer statische bestanden uit /public
-        app.use(express.static(path.join(__dirname, "public")));
+        // Static bestanden
+        app.use("/mybirthdays", express.static(path.join(__dirname, "public")));
 
         // Homepage
-        app.get("/", (req, res) => {
+        app.get("/mybirthdays", (req, res) => {
             res.sendFile(path.join(__dirname, "public", "index.html"));
         });
 
         // Data ophalen
-        app.get("/api/birthdays", (req, res) => {
+        app.get("/mybirthdays/api/birthdays", (req, res) => {
             res.json(this.data);
         });
 
         // Data opslaan
-        app.post("/api/birthdays", (req, res) => {
-            this.data = req.body;
+        app.post("/mybirthdays/api/birthdays", (req, res) => {
+            this.data = Array.isArray(req.body) ? req.body : [];
             fs.writeFileSync(this.filePath, JSON.stringify(this.data, null, 2));
-            // Realtime update MagicMirror
-            this.sendSocketNotification("BIRTHDAYS_LOADED", this.data);
-            res.json({ status: "ok" });
-        });
 
-        app.listen(3123, () => {
-            console.log("MMM-MyBirthdays webserver op poort 3123");
+            // realtime update mirror
+            this.sendSocketNotification("BIRTHDAYS_LOADED", this.data);
+
+            res.json({ status: "ok" });
         });
     },
 
